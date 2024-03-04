fd756b349e8006e90658567fbfc48c7f8f94e91c
DiogoRocha22 <diogorocha2906@gmail.com>
2024-03-04 15:29:11 -0300

    setup previsao tempo

---
+++ previsao_tempo/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.scss'
+import Busca from './components/Busca/Busca'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Condições Climáticas</h1>
+      <Busca/>
+    </div>
+  )
+}
+
+export default App
---
+++ previsao_tempo/src/components/Previsao/Previsao.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import 'index.scss'
+
+export default function Previsao() {
+  return (
+    <div>Previsao</div>
+  )
+}
